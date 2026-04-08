87178a9d627f4683858d5d09f7a8813f777f4745
vijayhayagrivan2004-creator <vijayhayagrivan2004@gmail.com>
2026-04-08 21:02:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,14 @@
-// SMOOTH SCROLL (LENIS)
-const lenis = new Lenis();
+const links = document.querySelectorAll("nav a");
+const pages = document.querySelectorAll(".page");
 
-function raf(time) {
-  lenis.raf(time);
-  requestAnimationFrame(raf);
-}
+links.forEach(link=>{
+  link.addEventListener("click", e=>{
+    e.preventDefault();
 
-requestAnimationFrame(raf);
+    const target = link.getAttribute("href");
 
+    pages.forEach(p=>p.classList.remove("active"));
 
-// GSAP HERO ANIMATION
-gsap.from("#title", {
-  y:100,
-  opacity:0,
-  duration:1.2
-});
-
-gsap.from("#subtitle", {
-  y:50,
-  opacity:0,
-  delay:0.3
-});
-
-gsap.from(".buttons a", {
-  y:30,
-  opacity:0,
-  stagger:0.2,
-  delay:0.5
-});
-
-
-// 3D TILT
-const img = document.getElementById("tilt");
-
-document.addEventListener("mousemove",(e)=>{
-  let x = (window.innerWidth/2 - e.clientX)/25;
-  let y = (window.innerHeight/2 - e.clientY)/25;
-  img.style.transform = `rotateY(${x}deg) rotateX(${y}deg)`;
+    document.querySelector(target).classList.add("active");
+  });
 });
